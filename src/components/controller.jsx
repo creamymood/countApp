7453236bce42394d7b74e3a84c1dfd07d0a9191d
creamymood <wbwbwlgid@naver.com>
2025-04-18 14:44:59 +0900

    뷰어와 컨트롤러 컴포넌트 구성

---
+++ src/components/controller.jsx
@@ -0,0 +1,17 @@
+const Controller = () => {
+
+
+    return (
+        <>
+            <button>-10</button>
+            <button>-100</button>
+            <button>-1000</button>
+            <button>+1000</button>
+            <button>+100</button>
+            <button>+1</button>
+
+        </>
+    )
+}
+
+export default Controller;
